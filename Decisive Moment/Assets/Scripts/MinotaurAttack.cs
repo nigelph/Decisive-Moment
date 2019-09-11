b35c4c8c78174ff503cf7c1b6d30d612054fd598
JoshMallaby <54135723+JoshMallaby@users.noreply.github.com>
2019-09-12 11:45:05 +1200

    Scripts for patrolling and attacking player
    Scripts added to minotaur monster for patrolling solid ground and attacking the player if they are in front

---
+++ Decisive Moment/Assets/Scripts/MinotaurAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinotaurAttack : MonoBehaviour
+{
+    //Code is entered upon the object entering the player character's box collider
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Checks that the box collider entered is the player's
+        if (col.gameObject.name.Equals("Player"))
+        {
+            //Sets 'isAttacking' boolean variable within 'MinotaurPatrol' script to true, starting the attack animation
+            MinotaurPatrol.isAttacking = true;
+        }
+
+    }
+    //Code is entered when the attack check object exits a collider
+    void OnTriggerExit2D(Collider2D col)
+    {
+        //Checks that the box collider exited is the player's
+        if (col.gameObject.name.Equals("Player"))
+        {
+            //Sets 'isAttacking' boolean variable within 'MinotaurPatrol' script to false, stopping the attack animation
+            MinotaurPatrol.isAttacking = false;
+        }
+    }
+}
