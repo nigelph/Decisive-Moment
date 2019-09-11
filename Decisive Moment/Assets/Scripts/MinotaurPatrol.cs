68e86e20ec772356b09efe9cfb59f9148fce1564
JoshMallaby <54135723+JoshMallaby@users.noreply.github.com>
2019-09-11 17:28:40 +1200

    Created scripts for patrolling and attacking as well as making changes to the terrain
    Added scripts to the minotaur monster to allow it to patrol back and forth along platforms without walking off. Also added scripts to allow it to enter its attack animation. Changes were also made to the terrain to make it less buggy.

---
+++ Decisive Moment/Assets/Scripts/MinotaurPatrol.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinotaurPatrol : MonoBehaviour
+{
+    //Dictates how fast the enemy moves
+    public float speed;
+    //Checks direction
+    private bool movingRight = true;
+    //Checks if the monster is currently attacking
+    public static bool isAttacking = false;
+
+    Animator anim;
+
+    public Transform groundDetection;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gets the monster's animator component
+        anim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moves the monster by a set speed
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        //Sends a ray downward from the groundInfo object to check if there is ground underneath where the monster is heading
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+
+        //If there is no ground ahead
+        if(groundInfo.collider == false)
+        {
+            //Change direction to face left
+            if(movingRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            //Change direction to face right
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+        //Sets the boolean condition 'isAttacking' within the monster's animator component
+        if(isAttacking)
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+        
+    }
+    void FixedUpdate()
+    {
+
+
+    }
+}
---
+++ Decisive Moment/Assets/Scripts/Minotaur_Move.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minotaur_Move : MonoBehaviour
+{
+    float xDirection;
+
+    [SerializeField]
+    float speed = 3f;
+
+    Rigidbody2D rb;
+
+    bool facingRight = false;
+
+    Vector3 localScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localScale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+        xDirection = -1f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -9f)
+        {
+            xDirection = 1f;
+        }
+        else if(transform.position.x > 9f)
+        {
+            xDirection = -1f;
+        }
+        
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(xDirection * speed, 0);
+    }
+    void LateUpdate()
+    {
+        FaceDirection();
+    }
+    void FaceDirection()
+    {
+        if(xDirection > 0)
+        {
+            facingRight = true;
+        }
+        else if(xDirection < 0)
+        {
+            facingRight = false;
+        }
+
+        if(((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+        {
+            localScale.x *= 1;
+        }
+
+        transform.localScale = localScale;
+    }
+}
