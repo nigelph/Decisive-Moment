3d569d6c7f360d5c540acdee27dd083ed73900a1
JoshMallaby <54135723+JoshMallaby@users.noreply.github.com>
2019-10-15 14:56:32 +1300

    Checkpoint flags change colour when reached
    Added script for changing colour of flag when reached. Added checkpoint to prefabs and created a new tag

---
+++ Decisive Moment/Assets/Scripts/CheckpointControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointControl : MonoBehaviour
+{
+    public Sprite redFlag;
+    public Sprite greenFlag;
+    private SpriteRenderer checkpointRenderer;
+    public bool reachedCheckpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkpointRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name.Equals("Player"))
+        {
+            checkpointRenderer.sprite = greenFlag;
+            reachedCheckpoint = true;
+        }
+    }
+}
