5c927e83dca2a052e61816cbfbcedb2485783421
JoshMallaby <54135723+JoshMallaby@users.noreply.github.com>
2019-10-22 18:09:05 +1300

    Added unit test script for MonsterDamageTest

---
+++ Decisive Moment/Assets/Editor/MonsterDamageTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class MonsterDamageTest
+{
+    [Test]
+    public void CalculateMinotaurHitpoints_Test()
+    {
+        /*//ARRANGE
+        var minotaurPatrol = new MinotaurPatrol();
+        var initialHitpoints = 1;
+        var damage = 7;
+
+        var expectedHitpoints = (0);
+
+        //ACT
+        var resultingHitpoints = minotaurPatrol.MinotaurHitPoints(damage, initialHitpoints);
+
+        //ASSERT
+        Assert.That(resultingHitpoints, Is.EqualTo(expectedHitpoints));*/
+    }
+}
