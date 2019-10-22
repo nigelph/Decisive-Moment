32ac9844735b0fbe2bfc3f1a6cbd3835552e9738
nigelph <54135755+nigelph@users.noreply.github.com>
2019-10-22 21:15:49 +1300

    Zane's Unit Test
    Zane's Unit test has been implemented into the latest build

--- Decisive Moment/Assets/Scripts/Initial_Option.cs
+++ Decisive Moment/Assets/Scripts/Initial_Option.cs
@@ -9,31 +9,52 @@ public class Initial_Option : MonoBehaviour
     public Transform PosOne;
     public Transform PosTwo;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update 
     void Start()
     {
-
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
+    {
+        StartGameScene();
+    }
+
+    public void StartGameScene()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
             selection = 1;
             transform.position = PosOne.position;
-        }else if (Input.GetKeyDown(KeyCode.S))
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
         {
             selection = 2;
             transform.position = PosTwo.position;
         }
-        if (selection==1 && Input.GetKeyDown(KeyCode.Space))
+
+        if (selection == 1 && Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(1);
         }
+
         if (selection == 2 && Input.GetKeyDown(KeyCode.Space))
         {
             Application.Quit();
         }
     }
+
+    /*
+    public char StartGameScene(char userSelection) 
+    { 
+        //1/2 condition 
+        char selection = userSelection; 
+        char properSelection = '2'; 
+        if (selection != properSelection) 
+        { 
+            return selection; 
+        }         
+        return properSelection; 
+    }
+    */
 }
