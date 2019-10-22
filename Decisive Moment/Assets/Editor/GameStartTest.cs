66444034b36ec8fd98b02e9678a57e50c8b186a1
nigelph <54135755+nigelph@users.noreply.github.com>
2019-10-22 21:31:45 +1300

    Zane Unit Test
    Zane's unit test has been implemented

---
+++ Decisive Moment/Assets/Editor/GameStartTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class GameStartTest
+{
+    [Test]
+    public void GameStart_Test()
+    {
+        /* 
+        //Arrange 
+        Initial_Option gameMenu = new Initial_Option(); 
+        //userSelection is the only char needed for change during a test 
+        char userSelection = '2'; 
+        char expectedSelection = '2'; 
+        //Act 
+        char testKey = gameMenu.StartGameScene(userSelection); 
+        //Assert 
+        Assert.That(testKey, Is.EqualTo(expectedSelection));
+        */
+    }
+}
--- Decisive Moment/Assets/Scripts/Initial_Option.cs
+++ Decisive Moment/Assets/Scripts/Initial_Option.cs
@@ -15,25 +15,49 @@ public class Initial_Option : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
+
     void Update()
+    {
+        StartGameScene();
+    }
+
+    public void StartGameScene()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
             selection = 1;
             transform.position = PosOne.position;
-        }else if (Input.GetKeyDown(KeyCode.S))
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
         {
             selection = 2;
             transform.position = PosTwo.position;
         }
-        if (selection==1 && Input.GetKeyDown(KeyCode.Space))
+
+        if (selection == 1 && Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(1);
         }
+    
         if (selection == 2 && Input.GetKeyDown(KeyCode.Space))
         {
             Application.Quit();
         }
     }
+
+    /* 
+    public char StartGameScene(char userSelection) 
+    { 
+        //1/2 condition 
+        char selection = userSelection;    
+        char properSelection = '2';
+        
+        if (selection != properSelection) 
+        { 
+            return selection; 
+        }         
+        return properSelection; 
+    } 
+    */
 }
